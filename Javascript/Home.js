1380d80542360cd24554771350fb3e8a84bc6bf8
Kelvin Adjei <kyriejnr2610@gmail.com>
2025-06-01 23:30:50 +0000

    desktop review section structure design complete

--- Javascript/Home.js
+++ Javascript/Home.js
@@ -36,3 +36,22 @@ window.addEventListener("scroll", ()=>{
     }
 });
 */
+
+/////////////////////REVIEW SECTION SEE MORE TOGGLE/////////////////////
+const reviewSection = document.querySelector(".js-review-cards-container");
+const seeMoreToggle = document.querySelector(".js-see-more");
+const seeMoreText = document.querySelector(".js-see-more-text");
+
+
+if(seeMoreText.innerHTML = `See more`){
+    seeMoreToggle.addEventListener("click", ()=>{
+    reviewSection.style.overflow = ('auto');
+    seeMoreText.innerHTML = `See less`;
+})
+}
+else if(seeMoreText.innerHTML = `See less`){
+    seeMoreToggle.addEventListener("click", ()=>{
+        reviewSection.style.overflow = ('hidden');
+        seeMoreText.innerHTML = `See more`;
+    })
+}
