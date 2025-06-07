6490c900b1013aef84ab2125fb60a4d36997ea6a
Kelvin Adjei <kyriejnr2610@gmail.com>
2025-06-07 18:31:45 +0000

    section 3 redesign

--- Javascript/Home.js
+++ Javascript/Home.js
@@ -1,41 +1,11 @@
+/////////////////////////////////CAROUSEL///////////////////////////////////
 
-/*const counters = document.querySelectorAll(".counter");
-const container = document.querySelector(".js-counter");
+const carousels = document.querySelectorAll('.carousel');
+carousels.forEach(carousel => {
+    const clone = carousel.innerHTML;
+    carousel.innerHTML += clone;
+})
 
-let activated = false;
-
-window.addEventListener("scroll", ()=>{
-    if(pageXOffset > container.offsetTop - container.offsetHeight - 200 && activated === false){
-        
-        counters.forEach(counter => {
-            counter.innerText = 0;
-            let count = 0;
-
-            function updateCount(){
-                const target = parseInt(counter.dataset.count);
-
-                if(count < target){
-                    count++;
-                    count.innerText = count;
-                    setTimeout(updateCount, 10);
-                }
-                else{
-                    counter.innerText = target;
-                }
-            }
-            updateCount();
-            activated = true;
-        });
-    }
-    else if(pageYOffset < container.offsetTop - container.offsetHeight - 500 || pageYOffset === 0 && activated === true){
-            counters.forEach(counter => {
-            counter.innerText = 0;
-        })
-
-        activated = false;
-    }
-});
-*/
 
 /////////////////////REVIEW SECTION SEE MORE TOGGLE/////////////////////
 const reviewSection = document.querySelector(".js-review-cards-container");
