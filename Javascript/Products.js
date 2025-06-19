7fc9ae3cb8aac41c18847964e321b3781779b1c0
Kelvin Adjei <kyriejnr2610@gmail.com>
2025-06-19 10:08:24 +0000

    created wishlist popup

--- Javascript/Products.js
+++ Javascript/Products.js
@@ -10,7 +10,6 @@ openPopupButtons.forEach(button => {
         popup.classList.add('show');
     })
 })
-
 closePopup.addEventListener("click", ()=>{
     popup.classList.remove('show');
     popup.classList.add('closing');
@@ -20,4 +19,69 @@ closePopup.addEventListener("click", ()=>{
         popup.close();
     }, { once: true })
 })
+window.addEventListener('scroll', ()=>{
+    popup.classList.remove('show');
+    popup.classList.add('closing');
+
+    popup.classList.remove('closing');
+    popup.close();
+})
+
+
+
+/////////////////////////////////////WHISHLIST///////////////////////////////////////
+
+////////////WISHLIST POPUP/////////
+const wishlistPopup = document.querySelector(".wishlist-poup");
+const openWishlistBtn = document.querySelectorAll(".js-open-wishlist");
+const overlay = document.querySelector(".wishlist-popup-overlay");
+
+openWishlistBtn.forEach(button=>{
+    button.addEventListener('click', ()=>{
+        wishlistPopup.classList.remove('wishlist-hide');
+        wishlistPopup.classList.add('wishlist-show')
+
+        overlay.classList.remove('overlay-hide');
+        overlay.classList.add('overlay-show');
+
+        savedPopup.classList.remove('show-popup');
+        savedPopup.classList.add('hide-popup');
+    })
+})
+overlay.addEventListener('click', ()=>{
+    wishlistPopup.classList.remove('wishlist-show');
+    wishlistPopup.classList.add('wishlist-hide');
+
+    overlay.classList.remove('overlay-show');
+    overlay.classList.add('overlay-hide')
+})
+window.addEventListener('scroll', ()=>{
+    wishlistPopup.classList.remove('wishlist-show');
+    wishlistPopup.classList.add('wishlist-hide');
+
+    overlay.classList.remove('overlay-show');
+    overlay.classList.add('overlay-hide')
+})
+
+
+
+///////////SAVED POPUP////////////
+const addToWishlistBtn = document.querySelectorAll(".js-add-to-wishlist");
+const savedPopup = document.querySelector(".js-saved-popup");
+
+addToWishlistBtn.forEach(button => {
+    button.addEventListener('click', ()=>{
+        savedPopup.classList.remove('hide-popup');
+        savedPopup.classList.add('show-popup');
+        
+        if(savedPopup.classList.contains('show-popup')){
+            setTimeout(() => {
+                savedPopup.classList.remove('show-popup');
+                savedPopup.classList.add('hide-popup');
+            }, 5000)
+        }
+    })
+})
+
+
 
