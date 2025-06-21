6b122896328d3bc1bbeaf734591c0d6925b8dacd
Kelvin Adjei <kyriejnr2610@gmail.com>
2025-06-21 00:24:41 +0000

    search page incomplete

--- Javascript/Home.js
+++ Javascript/Home.js
@@ -6,6 +6,7 @@ carousels.forEach(carousel => {
     carousel.innerHTML += clone;
 })
 
+
 ///////////////////////////BACK TO TOP LINK FUNCTION//////////////////////////
 const backToTopButton = document.querySelector(".js-back-to-top-btn");
 
@@ -21,3 +22,32 @@ window.addEventListener("scroll", ()=> {
         }
     }
 })
+
+
+
+
+///////////////////////////////////SEARCH REDIRECT//////////////////////////////
+const searchBtn = document.querySelectorAll(".search-btn");
+const search = document.querySelectorAll(".search");
+const searchInput = search.value;
+
+searchBtn.forEach(button => {
+    button.addEventListener('click', ()=>{
+        searchRedirect();
+    })
+})
+search.forEach(Input => {
+    Input.addEventListener('keydown', (e)=>{
+        if(e.key == 'Enter'){
+            searchRedirect();
+        }
+    })
+})
+
+function searchRedirect(){
+    if (searchInput !== "") {
+        window.location.href = `SearchResults.html?q=${encodeURIComponent(searchInput)}`;
+        
+        searchInput = '';
+    }
+}
