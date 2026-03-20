40c122afe039b0f143108656172b8d45c08460ed
Kelvin Adjei <kyriejnr2610@gmail.com>
2026-03-20 23:29:19 +0000

    faq section redesign

--- Javascript/Home.js
+++ Javascript/Home.js
@@ -129,6 +129,23 @@ if(document.querySelector(".packages-swiper")){
 }
 
 
+//// CAROUSEL SLIDER
+// if(document.querySelector(".logo-carousel")){
+//     const logoCarousel = new Swiper('.logo-carousel', {
+//         slidesPerView: 'auto',
+//         spaceBetween: 20,
+//         loop: true,
+//         loopedSlides: 10, // Duplicate this many slides for smoother loop
+//         speed: 3000, // Slow, long transition
+//         autoplay: {
+//             delay: 0,
+//             disableOnInteraction: false,
+//             pauseOnMouseEnter: true,
+//         },
+//         allowTouchMove: false
+//     });
+// }
+
 
 
 
